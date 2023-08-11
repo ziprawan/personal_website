197db0046403aa4232e794bc2935a73a6f8ad47f
Aziz Ridhwan Pratama <azizpratama@gnuweeb.org>
2023-08-11 13:40:22 +0700

    layout: add google verification and it's env
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama@gnuweeb.org>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Ubuntu } from "next/font/google";
 import BodyLayout from "./body";
 import { DarkModeProvider } from "@/context/darkmode/darkmode";
 
+const GOOGLE_VERIFICATION = process.env.GOOGLE_VERIFICATION ?? "";
+
 const ubuntu = Ubuntu({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
@@ -13,6 +15,9 @@ const ubuntu = Ubuntu({
 export const metadata = {
   title: "Aziz's Website",
   description: "Aziz's website that powered by Next.js",
+  verification: {
+    google: GOOGLE_VERIFICATION,
+  },
 };
 
 export default function RootLayout({
