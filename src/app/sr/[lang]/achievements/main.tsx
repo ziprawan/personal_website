e0cf89636e8166224531bce607f83d3f9a5b2adb
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2024-06-18 14:07:27 +0700

    sr: Add achievement total at main page
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- src/app/sr/[lang]/achievements/main.tsx
+++ src/app/sr/[lang]/achievements/main.tsx
@@ -1,12 +1,13 @@
 // import Series from Output/series.json";
+import Achievement from "#hsrOutput/achievements.json";
 import Series from "#hsrOutput/series.json";
 import { IMultiTextMap, ITextMap } from "@/types/textmap/text";
 import { readFileSync } from "fs";
 import SeriesCard from "./seriesCard";
 
 const map: IMultiTextMap = {
-  id: { 1: "Pilih salah satu seri achievement di bawah ini" },
-  en: { 1: "Select one of the achievement series below" },
+  id: { 1: "Pilih salah satu seri achievement di bawah ini", 2: "Total achievement saat ini" },
+  en: { 1: "Select one of the achievement series below", 2: "Achievement total for now is" },
 };
 
 export default function SRAchievementsMain({ langCode }: { langCode: string }) {
@@ -18,6 +19,9 @@ export default function SRAchievementsMain({ langCode }: { langCode: string }) {
   return (
     <div className="flex flex-col gap-3">
       <div className="text-lg">{map[lang][1]}:</div>
+      <div className="text-lg">
+        {map[lang][2]}: {Object.keys(Achievement).length}
+      </div>
       <div className="flex flex-col gap-1 ml-2">
         {Object.values(Series)
           .sort((a, b) => a.id - b.id)
